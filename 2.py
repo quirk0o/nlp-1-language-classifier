7ce464871104501f5da24daa438cd3f40411ab8b
Beata Obrok <beata.obrok@u2i.com>
2017-03-08 14:30:51 +0100

    Working caching of language vectors

--- 2.py
+++ 2.py
@@ -2,6 +2,9 @@ import os
 import pickle
 import sys
 import json
+
+from collections import defaultdict
+
 from classifier import *
 from ngram_builder import build_language_vector
 
@@ -19,15 +22,18 @@ if __name__ == '__main__':
         cache_file = 'cache/{}{}.data'.format(language.lower(), n)
         if os.path.exists(cache_file):
             with open(cache_file) as cache:
-                data['vec'] = LVec(vec=pickle.load(cache))
+                data['vec'] = LVec(vec=defaultdict(lambda: 0.0, pickle.load(cache)))
         else:
             vec = build_language_vector(n, data['samples'])
             data['vec'] = vec
 
-            # with open(cache_file, 'wb') as cache:
-            #     pickle.dump(vec.vec, cache)
+            with open(cache_file, 'wb') as cache:
+                pickle.dump(vec.vec.items(), cache)
 
     classifier = LanguageClassifier(n, languages)
 
     while True:
-        print classifier.determine_language(raw_input('> '))
+        input_text = raw_input('> ')
+        if input_text == '':
+            continue
+        print classifier.determine_language(input_text)
